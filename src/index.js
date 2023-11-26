1224a7ce180b59b836f5dc0031dc5f05423ae6c9
Matthew Montejo <montejododger@yahoo.com>
2023-11-25 17:57:06 -0800

    on Routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+};
+
+const renderApp = () => {
+    root.render(
+        <React.StrictMode>
+            <Root />
+        </React.StrictMode>
+    );
+};
+
+renderApp();
